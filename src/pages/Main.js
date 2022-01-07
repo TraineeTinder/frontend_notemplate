f226483680a68abd2504e670867f7798e9adefaf
Victor Rocha <Victor.rocha2002@gmail.com>
2022-01-07 14:16:57 -0500

    criacao front(pasta rotas e main)

---
+++ src/pages/Main.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import{View, Text} from 'react-native';
+
+export default function Main(){
+    return <View>
+        <Text>Pagina de likes dos clientes</Text>
+    </View>
+}
---
+++ src/routes.js
@@ -0,0 +1,10 @@
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
+import Main from './pages/Main';
+
+const Routes = createAppContainer(
+    createSwitchNavigator({
+        Main//pagina de likes de clientes
+    })
+);
+
+export default Routes;
