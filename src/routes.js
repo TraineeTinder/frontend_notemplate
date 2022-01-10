58fd2164c92e468063921521838b8a56ebc5d038
Victor Rocha <Victor.rocha2002@gmail.com>
2022-01-09 20:39:57 -0500

    criacao inicial card cliente

--- src/routes.js
+++ src/routes.js
@@ -1,6 +1,5 @@
 import { createAppContainer, createSwitchNavigator } from "react-navigation";
 import Main from './pages/Main';
-
 const Routes = createAppContainer(
     createSwitchNavigator({
         Main//pagina de likes de clientes
