2a035651db5be3f691f45548e5c261802cea6a45
Victor Rocha <Victor.rocha2002@gmail.com>
2022-01-24 19:39:51 -0500

    Organizacao View's e Scroll dentro do footer

--- src/pages/clients/InicioClientes.js
+++ src/pages/clients/InicioClientes.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import{SafeAreaView,View,Image, Text, StyleSheet, TouchableOpacity} from 'react-native';
+import{SafeAreaView,View,Image, Text, StyleSheet, TouchableOpacity, ScrollView} from 'react-native';
 import {FontAwesome} from 'react-native-vector-icons';
 import teste1 from '../../../assets/teste1.jpg';
 import teste from '../../../assets/teste.png';
@@ -9,32 +9,37 @@ import teste from '../../../assets/teste.png';
 
 export default function InicioClientes({navigation}){
     return( 
-    <SafeAreaView style={styles.container}>
-        <View style={styles.Cards}>
-            <View style={styles.ProfileCard}>
-                <Image style={styles.avatar} source={teste}/>
-                <View style={styles.footer}>
-                    <Text style={styles.name}>Victor</Text>
-                        <View style={styles.buttonbio}>
-                            <Text sytle={styles.infos}>Mais informações</Text>
-                            <Text sytle={styles.arrow}>v</Text>
+        <View style={styles.container}>
+            <View style={styles.Cards}>
+            <Text style={styles.name}>Victor</Text>
+                        <View style={styles.footer}>
+                            <ScrollView>
+                                <View style={styles.ProfileCard}>
+                                    <Image style={styles.avatar} source={teste}/>
+                                </View>
+                                <View style={styles.buttoninfos}>
+                                    <Text sytle={styles.infos}>Mais informações</Text>
+                                        <Text sytle={styles.arrow}>v</Text>
+                                </View>
+                                <View style = {styles.bio}>
+                                    <Text style={styles.titleBio}>Sobre mim:</Text> 
+                                    <Text> Lorem ipsum dolor sit amet, consectetur adipiscing elit. Cras volutpat odio a malesuada venenatis. Vestibulum dignissim rutrum est, at facilisis arcu finibus vitae. Donec varius, leo vel interdum fringilla, nisl turpis placerat est, sed consectetur risus purus vitae elit. Nulla suscipit, elit sed pretium tempor, libero quam lobortis nulla, eu ultricies sapien sem ac augue. Maecenas nec porttitor enim, sed blandit metus. Aenean vel convallis erat, ut dapibus massa. Nam iaculis nibh mi, et sollicitudin nisi pulvinar a. Maecenas tempor nec felis vitae lobortis. Fusce a mi lorem. Sed non sollicitudin enim. Cras tortor magna, imperdiet in vulputate eget, lacinia ac ex. Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas. Duis vitae arcu nisl. Nam metus elit, faucibus vel odio nec, finibus scelerisque libero. Sed at enim vitae orci facilisis gravida. </Text>
+                                </View>
+                                </ScrollView>
                         </View>
-                        <View style = {styles.scrow}> <Text> Olá Mundo! </Text></View>
-                </View>
+            </View> 
+            <View style={styles.buttonsContainer}>
+                <TouchableOpacity style={styles.buttonDeslike}>
+                    <FontAwesome name="times" size={70} color="#F06795"></FontAwesome>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonFilter}>
+                    <FontAwesome name="filter" size={40} color="#CDF820"></FontAwesome>
+                </TouchableOpacity>
+                <TouchableOpacity style={styles.buttonLike}>
+                    <FontAwesome name="check-circle" size={70} color="#1EDF25"></FontAwesome>
+                </TouchableOpacity>
             </View>
-        </View> 
-        <View style={styles.buttonsContainer}>
-            <TouchableOpacity style={styles.buttonDeslike}>
-                <FontAwesome name="times" size={70} color="#F06795"></FontAwesome>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.buttonFilter}>
-                <FontAwesome name="filter" size={40} color="#CDF820"></FontAwesome>
-            </TouchableOpacity>
-            <TouchableOpacity style={styles.buttonLike}>
-                <FontAwesome name="check-circle" size={70} color="#1EDF25"></FontAwesome>
-            </TouchableOpacity>
         </View>
-    </SafeAreaView>
     );
 }
 
@@ -52,12 +57,15 @@ const styles = StyleSheet.create({
         flex: 1,
         alignSelf:'stretch',
         justifyContent:'center',
-        maxHeight:480,
+        height: 380,
+        width: 350,
         backgroundColor: "#DDD"
     },
     Cards:{
+        paddingTop: 20,
+        marginTop: 10,
         borderWidth: 1,
-        borderColor: "#DDD",
+        borderColor: "#FFF",
         borderRadius: 8,
         margin: 8,
         overflow: 'hidden', 
@@ -69,34 +77,49 @@ const styles = StyleSheet.create({
     }, 
     avatar:{
         flex: 1,
-        height: 300,
+        height: 200,
+        borderRadius: 20,
+        borderColor: '#FFFF'
     },
     footer:{
+        height: 500,
+        marginTop: 60,
         backgroundColor: '#AEC2DF',
         paddingHorizontal: 10,
         paddingVertical: 5,
-        alignItems: 'center'
+        alignItems: 'center',
+        position: 'absolute'
     },
     name:{
-        fontSize: 26,
+        fontSize: 30,
         fontWeight: 'bold',
-        color: '#000'
+        color: '#000',
+        marginLeft: 20
     },
-    buttonbio:{
+    buttoninfos:{
+        alignSelf:'center',
         borderWidth: 1,
         borderColor: "#DDD",
         borderRadius: 15,
         backgroundColor: "#DDD",
-        marginTop: 5,
         lineHeight: 20,
-        alignItems: 'center'
+        alignItems: 'center',
+    },
+    infos:{
+        color: '#1EDF25',
+        position: 'absolute'
+    },
+    arrow:{
+        color: '#1EDF25',
+        position: 'absolute'
     },
     buttonsContainer:{
         height: 75,
         flexDirection: 'row',
         justifyContent:'space-around',
         alignItems: 'center',
-        right: 15 
+        right: 15,
+        
     },
     buttonDeslike:{
         width: 90,
@@ -106,7 +129,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginHorizontal: 50,
-        marginTop: 1050,
+        marginTop: 1200,
         elevation: 2,
         shadowColor: '#F06795',
         shadowOpacity: 0.05,
@@ -124,7 +147,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginHorizontal: 50,
-        marginTop: 1050,
+        marginTop: 1200,
         elevation: 2,
         shadowColor: '#1EDF25',
         shadowOpacity: 0.05,
@@ -142,7 +165,7 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         marginHorizontal: 50,
-        marginTop: 1100,
+        marginTop: 1350,
         elevation: 2,
         shadowColor: '#CDF820',
         shadowOpacity: 0.05,
@@ -152,7 +175,17 @@ const styles = StyleSheet.create({
             height: 2,
             }
         },
-        scrow: {
+        bio: {
+            margin: 20,
+            borderWidth: 1,
+            borderColor: "#DDD",
+            borderRadius: 15,
+            backgroundColor: "#DDD",
             
+        },
+        titleBio:{
+            fontWeight: 'bold',
+            fontSize: 16,
+            textAlign: 'left'
         }
 })
